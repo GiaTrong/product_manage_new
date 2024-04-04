1389ce0d31e139d2bc28847d8abb5a7d0589fd8f
GiaTrong <hoanggiatrang01@gmail.com>
2024-04-04 16:26:31 +0700

    docs: Cập nhật file env Cấu hình cho những biến cần private

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 const express = require('express')
 // ROUTES
 const route = require("./routes/client/index.route")
+// ENV
+require('dotenv').config();
 // app is the biggest file
 const app = express()
 // port: 
-const port = 3000
+const port = process.env.PORT
 
 // PUG
 app.set('views', './views') 
