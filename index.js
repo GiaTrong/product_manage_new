9ed6c4f0fac248043e16a8ec355d5a686f77dd79
GiaTrong <hoanggiatrang01@gmail.com>
2024-04-04 20:27:48 +0700

    feat: Kết nối database mongoDB

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express')
 const route = require("./routes/client/index.route")
 // ENV
 require('dotenv').config();
+// DATABASE
+const mongoose = require("mongoose")
 
 // app is the biggest file
 const app = express()
@@ -13,6 +15,9 @@ const port = process.env.PORT
 // PUG
 app.set('views', './views') 
 app.set('view engine', 'pug')
+
+// DATABASE
+mongoose.connect(process.env.MONGO_URL)
 
 // STATIC FILE
 app.use(express.static('public'))
