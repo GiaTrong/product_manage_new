0a1fa78e93978e7a02f1525d8b215c879c910b4b
GiaTrong <hoanggiatrang01@gmail.com>
2024-04-04 21:19:00 +0700

    feat: Danh sách sản phẩm bên trang client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,21 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model")
+
+module.exports.index = async (req, res) => {
+  // find({CONDITION}) => find all products which fit(phù hợp) with condition
+  const products = await Product.find({
+    status: "active",
+    deleted: false,
+  });
+
+  // console.log(products)
+
+  const newProducts = products.map(item => {
+    item.priceNew = Math.floor(item.price * (100 - item.discountPercentage) / 100);
+    return item;
+  })
+
     res.render("client/pages/products/index.pug", {
-        pageTitle: "Trang products"
+        pageTitle: "Trang products",
+        products: newProducts
       });
 }
