0379b43ab0a4b6e87ed7be7fd02b687d05600912
GiaTrong <hoanggiatrang01@gmail.com>
2024-04-05 03:32:17 +0700

    fix: Fix bug thay đổi button và Tối ưu phần lọc sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,12 +2,41 @@ const Product = require("../../models/product.model");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
+  // Buttons for user click to filter follow status
+  let filterStatus = [
+    {
+      name: "Tất cả",
+      status: "",
+      class: "active"
+    },
+    {
+      name: "Hoạt động",
+      status: "active",
+      class: ""
+    },
+    {
+      name: "Dừng hoạt động",
+      status: "inactive",
+      class: ""
+    },
+  ]
+
   let find = {
     deleted: false,
   };
 
   if(req.query.status) {
+    // filter DATA follow params
     find.status = req.query.status;
+
+    // CHANGE active
+    filterStatus.forEach(item => {
+      if(item.status == req.query.status) {
+        item.class = "active";
+      } else {
+        item.class = "";
+      }
+    })
   }
 
   // GIVE product
@@ -18,6 +47,6 @@ module.exports.index = async (req, res) => {
   res.render("admin/pages/products/index.pug", {
     pageTitle: "Trang quản lí sản phẩm",
     products: products,
-    status: req.query.status
+    filterStatus: filterStatus
   });
 };
