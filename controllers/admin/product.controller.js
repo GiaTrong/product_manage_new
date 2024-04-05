e8e353a1e32a88471d075f2f028c2fefb1ed04ff
GiaTrong <hoanggiatrang01@gmail.com>
2024-04-05 14:18:17 +0700

    refactor: Tối ưu phần search backend

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,42 +1,40 @@
 const Product = require("../../models/product.model");
-const filterStatusHelper = require('../../helpers/filterStatus')
+const filterStatusHelper = require("../../helpers/filterStatus");
+const searchHelper = require("../../helpers/search");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-
   // FILTER STATUS
   const filterStatus = filterStatusHelper(req);
-  
+
   // CONDITION of filter DATA
   let find = {
     deleted: false,
   };
 
-  if(req.query.status) {
+  if (req.query.status) {
     // filter DATA follow params
     find.status = req.query.status;
   }
 
   // SEARCH BY KEYWORD
-  // fix cứng 
+  const objSearch = searchHelper(req);
+
   let keyword = "";
   if(req.query.keyword) {
-    keyword = req.query.keyword
+    keyword =  objSearch.keyword;
 
-    // use REGEX for searching DATA which similar keyword
-    const regex = new RegExp(keyword, "i");
-
-    find.title = regex
+    find.title = objSearch.regex;
   }
+  
 
   // GIVE product by FIND
   const products = await Product.find(find);
-
 
   res.render("admin/pages/products/index.pug", {
     pageTitle: "Trang quản lí sản phẩm",
     products: products,
     filterStatus: filterStatus,
-    keyword: keyword
+    keyword: keyword,
   });
 };
---
+++ helpers/search.js
@@ -0,0 +1,14 @@
+module.exports = (req) => {
+  let objSearch = {
+    keyword: "",
+  };
+
+  if (req.query.keyword) {
+    objSearch.keyword = req.query.keyword;
+
+    const regex = new RegExp(objSearch.keyword, "i");
+    objSearch.regex = regex;
+  }
+
+  return objSearch;
+};
