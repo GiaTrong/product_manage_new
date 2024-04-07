d9e58e8b11099b0d75321bf4e251db3393c2e227
GiaTrong <hoanggiatrang01@gmail.com>
2024-04-07 14:50:31 +0700

    refactor: Sort sản phẩm theo position - desc

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -44,6 +44,7 @@ module.exports.index = async (req, res) => {
   // .limit(number) => chỉ lấy tưng đấy sản phẩm thôi
   // skip: bỏ qua bao nhiêu sản phẩm
   const products = await Product.find(find)
+    .sort({position: "desc"})
     .limit(objectPagination.limitItem)
     .skip(objectPagination.skip);
 
