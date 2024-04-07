24cf83f903e8ada6c53dba2669371aaa02ba937d
GiaTrong <hoanggiatrang01@gmail.com>
2024-04-07 20:57:47 +0700

    refactor: Thêm trường slug vào cho tính năng tạo mới Để hỗ trợ cho việc cell google tốt hơn

--- models/product.model.js
+++ models/product.model.js
@@ -1,23 +1,35 @@
 const mongoose = require("mongoose");
 
+// mongoose-slug-updater
+const slug = require("mongoose-slug-updater");
+mongoose.plugin(slug);
+
 // timestamps : Khi chỉnh sửa 1 trường nào đó, nó sẽ tự động chỉnh TIME
-const productSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  price: Number,
-  discountPercentage: Number,
-  stock: Number,
-  thumbnail: String,
-  status: String,
-  position: Number,
-  deleted: {
-    type: Boolean,
-    default: false,
+const productSchema = new mongoose.Schema(
+  {
+    title: String, // san pham 1
+    description: String,
+    price: Number,
+    discountPercentage: Number,
+    stock: Number,
+    thumbnail: String,
+    status: String,
+    position: Number,
+    slug: {
+        type: String,
+        slug: "title", //san-pham-1
+        unique: true,
+      },
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    deleteAt: Date,
   },
-  deleteAt: Date,
-}, {
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 const Product = mongoose.model("Product", productSchema, "products");
 
 module.exports = Product;
