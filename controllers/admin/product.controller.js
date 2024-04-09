fe9d57e7d25c8d9dfc470632554326cc8522966b
GiaTrong <hoanggiatrang01@gmail.com>
2024-04-09 22:50:15 +0700

    feat: Tính năng upload ảnh Chưa fix được bug: Mở ảnh trên url => tự động tải về

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -149,6 +149,8 @@ module.exports.createPost = async (req, res) => {
     req.body.position = parseInt(req.body.position);
   }
 
+  req.body.thumbnail = `/uploads/${req.file.filename}`
+
   // create new Product
   const product = new Product(req.body);
   // saving in DATABASE
--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,7 +1,14 @@
 const express = require('express')
+// UPLOAD IMAGE
+const multer  = require('multer')
 const route = express.Router();
 
+const upload = multer({ dest: "./public/uploads/" })
+// Đường dẫn đến FOLDER lưu trữ ảnh - Bởi vì đứng từ thằng multer(trong file NODE MODULE 
+// => ĐỨNG từ file CẤP CAO NHẤT RỒI => chỉ cần ./public/uploads nữa thôi       )
+
 const controller = require("../../controllers/admin/product.controller")
+
 route.get('/', controller.index);
 
 route.patch ('/change-status/:status/:id', controller.changeStatus);
@@ -12,6 +19,6 @@ route.delete ('/delete/:id', controller.deleteItem);
 
 route.get ('/create', controller.create);
 
-route.post ('/create', controller.createPost);
+route.post ('/create', upload.single('thumbnail'), controller.createPost);
 
 module.exports = route;
