e4b03d6742aeca21b994f6b5efe8947a50adfad2
GiaTrong <hoanggiatrang01@gmail.com>
2024-04-10 02:46:45 +0700

    fix: Fix bug - khi người dùng ko up ảnh lên là died luôn server

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -149,7 +149,9 @@ module.exports.createPost = async (req, res) => {
     req.body.position = parseInt(req.body.position);
   }
 
-  req.body.thumbnail = `/uploads/${req.file.filename}`;
+  if (req.file) {
+    req.body.thumbnail = `/uploads/${req.file.filename}`;
+  }
 
   // create new Product
   const product = new Product(req.body);
