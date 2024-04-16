773d45fd1a66d92d48462a97bdbe84f114da2eae
GiaTrong <hoanggiatrang01@gmail.com>
2024-04-16 09:59:13 +0700

    refactor: Fomat code

--- index.js
+++ index.js
@@ -1,55 +1,58 @@
 // import express
-const express = require('express')
+const express = require("express");
 // path
-const path = require('path');
+const path = require("path");
 // Cookie-parser
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 // Express-session
-const session = require('express-session')
+const session = require("express-session");
 // METHOD OVERRIDE
-const methodOverride = require('method-override')
+const methodOverride = require("method-override");
 // BODY PARSER
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 // Express Flash
-const flash = require('express-flash')
+const flash = require("express-flash");
 
 // ROUTES
-const route = require("./routes/client/index.route")
-const routeAdmin = require("./routes/admin/index.route")
+const route = require("./routes/client/index.route");
+const routeAdmin = require("./routes/admin/index.route");
 // ENV
-require('dotenv').config();
+require("dotenv").config();
 // DATABASE
-const database = require("./config/database")
+const database = require("./config/database");
 // system config
-const systemConfig = require("./config/system")
+const systemConfig = require("./config/system");
 // app is the biggest file
-const app = express()
-// port: 
-const port = process.env.PORT
+const app = express();
+// port:
+const port = process.env.PORT;
 
 // Express Flash
-app.use(cookieParser('Absafsdfsdaf'));
-app.use(session({ cookie: { maxAge: 60000 }}));
+app.use(cookieParser("Absafsdfsdaf"));
+app.use(session({ cookie: { maxAge: 60000 } }));
 app.use(flash());
 
 // TINYMCE
-app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
+app.use(
+  "/tinymce",
+  express.static(path.join(__dirname, "node_modules", "tinymce"))
+);
 
 // override with POST having ?_method=DELETE
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // PUG
-app.set('views', `${__dirname}/views`) 
-app.set('view engine', 'pug')
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "pug");
 
 // CONNECT DATABSE
 database.connect();
 
 // STATIC FILE
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
 //  APP VARIABLES LOCALS
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
@@ -58,7 +61,6 @@ app.locals.prefixAdmin = systemConfig.prefixAdmin;
 route(app);
 routeAdmin(app);
 
-
 app.listen(port, () => {
-  console.log(`Example app listening on port: localhost:${port}`)
-})
+  console.log(`Example app listening on port: localhost:${port}`);
+});
