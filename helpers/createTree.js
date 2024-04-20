c10170b8e3e1cd9eb557212c549723d17b7c641d
GiaTrong <hoanggiatrang01@gmail.com>
2024-04-20 08:07:26 +0700

    fix: Fix bug không tăng được STT trong danh mục sản phẩm STT: Số thứ tự

--- helpers/createTree.js
+++ helpers/createTree.js
@@ -1,8 +1,12 @@
-function createTree(arr, parentId = "") {
+let count = 0; // biến toàn cục
+
+const createTree = (arr, parentId = "") => {
   const tree = [];
   arr.forEach((item) => {
     if (item.parent_id == parentId) {
+      count++;
       const newItem = item;
+      newItem.index = count;
       const children = createTree(arr, item.id);
 
       if (children.length > 0) {
@@ -14,9 +18,12 @@ function createTree(arr, parentId = "") {
   });
 
   return tree;
-}
+};
 
 module.exports.createTree = (arr, parentId = "") => {
+  count = 0; // nếu ở đây ko gán lại count = 0; => mỗi khi load lại trang, biến count toàn cục
+             // sẽ không được khởi động lại => tăng mãi
+
   const tree = createTree(arr, (parentId = ""));
 
   return tree;
