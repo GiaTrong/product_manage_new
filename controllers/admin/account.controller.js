e46692b3ff5982a380306a30a3bedb9052df0841
GiaTrong <hoanggiatrang01@gmail.com>
2024-05-03 22:33:30 +0700

    fix: Thêm check EMAIL EXIST trong khi chỉnh sửa sản phẩm

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -105,19 +105,33 @@ module.exports.edit = async (req, res) => {
   // [PATCH] /admin/accounts/edit/:id
 module.exports.editPatch = async (req, res) => {
     try {
-        if(req.body.password) {
-            req.body.password = md5(req.body.password);
+        // CHECK EMAIL
+        const id = req.params.id;
+        const emailExist = await Account.findOne({
+            _id: { $ne: id }, // tìm những thằng có id KHÁC id truyền vào 
+            email: req.body.email,
+            deleted: false,
+        })
+
+        if(emailExist) {
+            req.flash("error", `Email ${req.body.email} đã tồn tại`);
         } else {
-            delete req.body.password;
+            if(req.body.password) {
+                req.body.password = md5(req.body.password);
+            } else {
+                delete req.body.password;
+            }
+    
+            await Account.updateOne({_id: req.params.id}, req.body)
+    
+            req.flash("success", "Thay đổi tài khoản thành công");
+    
+            res.redirect(`${systemConfig.prefixAdmin}/accounts`);
         }
-
-        await Account.updateOne({_id: req.params.id}, req.body)
-
-        req.flash("success", "Thay đổi tài khoản thành công");
-
-        res.redirect(`${systemConfig.prefixAdmin}/accounts`);
     } catch (error) {
         console.log("Err in role.controller.js");
-        req.flash("error", "Tạo tài khoản thất bại");
+        req.flash("error", "Chỉnh sửa tài khoản thất bại");
     }
+
+    res.redirect("back");
 };
