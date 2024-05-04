465aca864e9bfa0247d79eea3c7da9a52dd4a611
GiaTrong <hoanggiatrang01@gmail.com>
2024-05-04 09:45:29 +0700

    feat: Tính năng phân quyền cho từng Account (Autherriaztion) Phân quyền cho PRODUCT_CATEGORY

--- middlewares/admin/auth.middlewares.js
+++ middlewares/admin/auth.middlewares.js
@@ -1,4 +1,5 @@
 const Account = require("../../models/accounts.model");
+const Role = require("../../models/roles.model");
 
 const systemConfig = require("../../config/system");
 
@@ -10,11 +11,22 @@ module.exports.requireAuth = async (req, res, next) => {
     const user = await Account.findOne({
       token: req.cookies.token,
       deleted: false,
-    });
+    }).select("-password");
 
     if (!user) {
       res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
     } else {
+      console.log("login success");
+      
+      const role = await Role.findOne({
+        _id: user.role_id,
+        deleted: false,
+      }).select("title permissions");
+      
+      // trả về thông tin của tài khoản: role là gì, làm được gì, ....
+      res.locals.user = user;
+      res.locals.role = role;
+
       //
       next();
     }
