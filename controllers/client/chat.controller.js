97a3363105278420bab1311f84ef22c4e02ffe14
GiaTrong2003 <96738746+tronghghe172557@users.noreply.github.com>
2024-05-13 23:00:16 +0700

    feat: Làm tính năng chat cơ bản
    Chưa có real time

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,12 +1,45 @@
+const Chat = require("../../models/chat.model");
+const User = require("../../models/users.model");
+
 // [GET] /chat/
 module.exports.index = async (req, res) => {
+  const userId = res.locals.user.id; // lấy bên authen
+
   // socketIO
-  _io.on("connection", (socket) => {
-    console.log("a user connected", socket.id);
+  // sự kiện  => khi có người đăng nhập vào
+  // on: cứ mỗi lần => load lại trang là kết nối mới
+  // once: kết nối 1 LẦN DUY NHẤT khi vào
+  _io.once("connection", (socket) => {
+    // lắng nghe sự kiện bên client gửi sang
+    socket.on("CLIENT_SEND_MESSAGE", async (content) => {
+      // console.log(content);
+      // console.log(userId);
+
+      const chat = new Chat({
+        user_id: userId,
+        content: content,
+      });
+
+      await chat.save();
+    });
   });
+
+  // lấy ra tất cả data
+  const chats = await Chat.find({
+    deleted: false,
+  });
+
+  for (const chat of chats) {
+    const infoUser = await User.findOne({ _id: chat.user_id }).select(
+      "fullName"
+    );
+
+    chat.infoUser = infoUser;
+  }
+
 
   res.render("client/pages/chat/index.pug", {
     pageTitle: "Trang chu",
+    chats: chats,
   });
 };
- 
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,16 @@
-const socket = io();
+// CLIENT_SEND_MESSAGE
+const formsendData = document.querySelector(".chat .inner-form");
+if (formsendData) {
+  formsendData.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const content = e.target.content.value;
+    if (content) {
+      // emit events: sự kiện trả về hoặc gửi đi của socketIO
+      // socket ấy ở trong file socket.js
+      socket.emit("CLIENT_SEND_MESSAGE", content);
+      e.target.content.value = "";
+    }
+  });
+}
+// END CLIENT_SEND_MESSAGE
